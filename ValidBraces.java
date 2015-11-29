b72381c49d9c8978607ec708b209331bc719e8d4
Ajay <cjajay93@gmail.com>
2015-11-29 00:46:22 -0600

    Update ValidBraces.java
    Above code checks for String (braces) validity.

--- ValidBraces.java
+++ ValidBraces.java
@@ -1,1 +1,34 @@
 import java.util.*;
+public class ValidBraces{
+  public static void main(String[] args){
+    String totest = "{[()]}";
+    System.out.print(validity(totest));
+  }
+  
+  public static boolean validity(String string){
+    HashMap<Character, Character> map = new HashMap<Character, Character>();
+    map.put('(', ')');
+    map.put('[', ']');
+    map.put('{', '}');
+    
+    Stack<Character> stack = new Stack<>();
+    
+    for(int i = 0; i < string.length(); i++){
+    	char para = string.charAt(i);
+    	if(map.keySet().contains(para)){
+    		stack.push(para);
+       	}
+    	else if(map.values().contains(para)){
+    		if(!stack.empty() && map.get(stack.peek())==para){
+    			stack.pop();
+    		}
+    		else{
+    			return false;
+    		}
+    		
+    	}
+    }
+	return stack.empty();
+    
+  }
+}
